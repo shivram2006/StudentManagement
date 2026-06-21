1e17f471c5fbae3ae43059979ca8a9f6b2394428
shivram2006 <sharmashivram2006@gmail.com>
2026-06-21 12:01:31 +0530

    Fix static settings for Render

--- studynest/settings.py
+++ studynest/settings.py
@@ -68,6 +68,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
